f27ef86481adf80a7be08b1263479b23f49ef51a
stoneface86 <44489054+stoneface86@users.noreply.github.com>
2020-06-07 02:56:27 -0400

    implement call, jump, ret and tempo commands

--- lib/commands.asm
+++ lib/commands.asm
@@ -1,6 +1,14 @@
 ; -----------------------------------------------------------------------------
 ; Command functions
 
+; command calling convention
+;  a - channel index (0, 1, 2 or 3)
+;  b - parameter 1
+;  c - parameter 2
+;  d - parameter 3
+;  hl - program counter (can be modified by commands)
+;  all registers except a and hl can be trashed
+
 cmdFnNote:
     ret
 
@@ -17,15 +25,49 @@ cmdFnHalt:
     ret
 
 cmdFnCall:
-    ret 
+    ; parameters 1+2 (bc) -> address to call
+    push    af
+    ld      d, h                ; de = current pc
+    ld      e, l
+    ld      hl, ch1Ret          ; hl = return address variable
+    add     a, l                ; offset by channel id
+    ld      l, a
+    ld      a, d
+    ld      [hl+], a            ; store de to the variable
+    ld      [hl], e
+    ld      h, d                ; restore hl
+    ld      l, e
+    pop     af                  ; restore a
+    ; fall-through to jump command
 
 cmdFnJump:
+    ; parameters 1+2 (bc) -> address to jump to
+    ld      l, b                ; set hl = bc (engine stores hl as the program counter)
+    ld      h, c
     ret 
 
 cmdFnRet:
+    ; since the engine will store hl into the program counter variable, we
+    ; just need to set hl to the return address variable
+    ld      hl, ch1Ret          ; hl = return address variable
+    add     a, l
+    ld      a, [hl+]            ; store into registers a and b
+    ld      b, [hl]
+    ld      h, b                ; store back into hl
+    ld      l, a
     ret
 
 cmdFnTempo:
+    ; parameter 1, a - new speed to set
+    ; check if a is >= 8 and < $F8
+    cp      a, $8
+    jp      c, .exit            ; no-op if new speed < $8 (1.0)
+    cp      a, $F8
+    jp      nc, .exit           ; no-op if new speed >= $F8 (31.0)
+    ld      [timerPeriod], a    ; set the new period
+    xor     a                   ; clear the current timer
+    ld      [timer], a
+.exit:
     ret
 
 cmdFnSfx:
--- lib/wram.asm
+++ lib/wram.asm
@@ -22,6 +22,16 @@ SECTION "tbengine_wram", WRAM0
 
 timer:          DS 1        ; current timer value
 timerPeriod:    DS 1        ; number of frames per row in Q5.3 format
+; low byte of these addresses MUST be <= 0xFC
+ch1Pc:          DS 2
+ch2Pc:          DS 2
+ch3Pc:          DS 2
+ch4Pc:          DS 2
+ch1Ret:         DS 2        ; return address for channel 1
+ch2Ret:         DS 2
+ch3Ret:         DS 2
+ch4Ret:         DS 2
+
 
     FreqControlStruct 1     ; frequency control for CH1
     FreqControlStruct 2     ; CH2
